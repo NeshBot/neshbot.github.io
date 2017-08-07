4e2d4ed48d223717a03345c305aabd4063eb2940
NeshBot <30552935+NeshBot@users.noreply.github.com>
2017-08-07 12:51:10 -0300

    Update info.js

--- info.js
+++ info.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-$.getJSON('https://ajax.service.maath.ga/data.php?id=286259539529957388', function(data) {
+$.getJSON('https://api.maath.ga/get/data/286259539529957388/', function(data) {
     $('#info').html('Uptime: '+data.uptime+ ' <br> Servidores: '+data.guilds)
 });
 });
