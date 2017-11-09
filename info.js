5e54c8bdc403b6814489d6e79c27402796e543b5
NeshBot <30552935+NeshBot@users.noreply.github.com>
2017-11-09 15:44:47 -0200

    Update info.js

--- info.js
+++ info.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-$.getJSON('https://s0uth.heliohost.org/data.json', function(data) {
+$.getJSON('https://s0uth.heliohost.org/json.php', function(data) {
     $('#info').html('Uptime: '+data.uptime+ ' <br> Servidores: '+data.guilds + '<br> Usuários: '+ data.users + '<br> Vezes que me chamaram: '+data.calls)
 });
 });
